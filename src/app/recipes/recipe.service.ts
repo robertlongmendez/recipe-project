062ed423f56c2ea619b9a6b40bfd8b918052fd91
robertlongmendez <robertlongmendez@gmail.com>
2021-06-21 16:47:07 -0500

    Course project Http added
    created real-time database for recipe app in firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,26 +10,32 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
 
-  private recipes: Recipe[] = [
-    new Recipe('Meat Cakes',
-      'Traditional cakes',
-      'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
+  // private recipes: Recipe[] = [
+  //   new Recipe('Meat Cakes',
+  //     'Traditional cakes',
+  //     'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
 
-    new Recipe('Fish Cakes',
-      'Fun cakes',
-      'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
-      [
-        new Ingredient('Fun', 200),
-        new Ingredient('More Fun', 300)
-      ])
+  //   new Recipe('Fish Cakes',
+  //     'Fun cakes',
+  //     'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
+  //     [
+  //       new Ingredient('Fun', 200),
+  //       new Ingredient('More Fun', 300)
+  //     ])
 
-  ];
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
